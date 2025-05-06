9caf5b63ddb845d39fc557589d3fdef87f62af7e
nffdev <nightfrost2discord@outlook.fr>
2025-05-06 22:21:52 +0200

    added function to hook shellcode

--- NoCapture/main.cpp
+++ NoCapture/main.cpp
@@ -16,3 +16,58 @@ BYTE _affinitySet[] = {
     0x48, 0x83, 0xC4, 0x28,                         // add rsp, 0x28
     0xC3                                            // ret
 };
+
+void SetAffinity(HANDLE procHandle, HWND lhWnd, DWORD lAffinity, FARPROC lpSetWindowDisplayAffinity) {
+    void* returnAddress = VirtualAllocEx(procHandle, NULL, 8, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (returnAddress == NULL) {
+        std::cerr << "Failed to allocate memory for return value in target process" << std::endl;
+        return;
+    }
+
+    for (int i = 0; i < 4; i++) {
+        _affinitySet[7 + i] = reinterpret_cast<BYTE*>(&lhWnd)[i];
+        _affinitySet[14 + i] = reinterpret_cast<BYTE*>(&lAffinity)[i];
+    }
+    for (int i = 0; i < 8; i++) {
+        _affinitySet[20 + i] = reinterpret_cast<BYTE*>(&lpSetWindowDisplayAffinity)[i];
+        _affinitySet[32 + i] = reinterpret_cast<BYTE*>(&returnAddress)[i];
+    }
+
+    void* addr = VirtualAllocEx(procHandle, NULL, sizeof(_affinitySet), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (addr == NULL) {
+        std::cerr << "Failed to allocate memory in the target process" << std::endl;
+        VirtualFreeEx(procHandle, returnAddress, 8, MEM_RELEASE);
+        return;
+    }
+
+    BOOL success = WriteProcessMemory(procHandle, addr, _affinitySet, sizeof(_affinitySet), NULL);
+    if (!success) {
+        std::cerr << "Failed to write to process memory" << std::endl;
+        VirtualFreeEx(procHandle, addr, sizeof(_affinitySet), MEM_RELEASE);
+        VirtualFreeEx(procHandle, returnAddress, 8, MEM_RELEASE);
+        return;
+    }
+
+    HANDLE thread = CreateRemoteThread(procHandle, NULL, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(addr), NULL, 0, NULL);
+    if (thread == NULL) {
+        std::cerr << "Failed to create remote thread" << std::endl;
+        VirtualFreeEx(procHandle, addr, sizeof(_affinitySet), MEM_RELEASE);
+        VirtualFreeEx(procHandle, returnAddress, 8, MEM_RELEASE);
+        return;
+    }
+
+    WaitForSingleObject(thread, INFINITE);
+    CloseHandle(thread);
+
+    BYTE returnValue;
+    success = ReadProcessMemory(procHandle, returnAddress, &returnValue, sizeof(BYTE), NULL);
+    if (!success) {
+        std::cerr << "Failed to read return value from process memory" << std::endl;
+    }
+    else {
+        std::cout << "SetWindowDisplayAffinity returned: " << (returnValue ? "TRUE" : "FALSE") << std::endl;
+    }
+
+    VirtualFreeEx(procHandle, addr, sizeof(_affinitySet), MEM_RELEASE);
+    VirtualFreeEx(procHandle, returnAddress, 8, MEM_RELEASE);
+}
