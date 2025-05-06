06c16196b2549c60bb00f00af743bd940b593802
nffdev <nightfrost2discord@outlook.fr>
2025-05-06 22:27:18 +0200

    added data struct

--- NoCapture/MyForm.h
+++ NoCapture/MyForm.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
+
+namespace NoCapture {
+    using namespace System;
+    using namespace System::ComponentModel;
+    using namespace System::Collections;
+    using namespace System::Windows::Forms;
+    using namespace System::Data;
+    using namespace System::Drawing;
+
+    struct EnumWindowsData {
+        DWORD processId;
+        std::vector<HWND> hwnds;
+    };
+}
